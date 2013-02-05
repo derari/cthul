ae09cd3c2810c6b72bdadb66bd9dc78dad87a219
Arian Treffer <arian.treffer@hpi.uni-potsdam.de>
2013-02-05 23:22:27 +0100

    xml: no logging if requested uri is null

--- xml/src/main/java/org/cthul/resolve/ObjectResolver.java
+++ xml/src/main/java/org/cthul/resolve/ObjectResolver.java
@@ -1,5 +1,6 @@
 package org.cthul.resolve;
 
+import java.net.URLClassLoader;
 import org.cthul.log.CLogger;
 import org.cthul.log.CLoggerFactory;
 
@@ -26,12 +27,12 @@ public abstract class ObjectResolver<T, E extends Exception> {
     protected T resolve(String uri, String publicId, String systemId, String baseURI) throws E {
         RRequest req = new RRequest(uri, publicId, systemId, baseURI);
         RResult res = resolver.resolve(req);
-
         if (res == null) {
-            log.warn("Could not resolve schema %s %if[as %<s]", uri, systemId);
+            if (uri != null) 
+                log.warn("Could not resolve schema %s %if[as %<s]", uri, systemId);
             return null;
         }
-
+        
         log.info("Resolved %s %if[as %<s]", uri, res.getSystemId());
         return result(res);
     }
