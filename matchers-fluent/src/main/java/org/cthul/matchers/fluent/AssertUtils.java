b56463d3371fe438456848c7d150a4fd73dbe114
at <arian.treffer@hpi.uni-potsdam.de>
2013-09-06 20:22:47 +0200

    removed matchers-fluent

--- matchers-fluent/src/main/java/org/cthul/matchers/fluent/AssertUtils.java
+++
@@ -1,9 +0,0 @@
-package org.cthul.matchers.fluent;
-
-import org.cthul.matchers.fluent.builder.AssertionErrorHandler;
-
-public class AssertUtils {
-
-    public static final AssertionErrorHandler ASSERT = AssertionErrorHandler.INSTANCE;
-
-}
